729e432453670dc00f63521ad39f96ffc4d8a665
ABHINAV SHARMA <97845012+hyperaid@users.noreply.github.com>
2023-08-26 15:08:19 +0530

    Time: 3 ms (76.55%) | Memory: 10.7 MB (49.51%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,47 +11,54 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* temp=head;
-        ListNode* temp1=head;
-        if(temp->next==NULL) return NULL;
-        int count=1;
-        while(temp->next->next!=NULL){
-            temp=temp->next;
-            count++;
-            // cout<<count<<endl;
-        }
-        int del=count-n+1;
-        int countt=1;
-        if((count+1)==n){ 
-            head=head->next;
-            
-            return head;}
-        if(n==1){
-            temp->next=NULL;
-            return head;
-        }
-        else {
-            while(countt!=del){
-                temp1=temp1->next;
-                 countt++;
-
-            }
-            temp1->next=temp1->next->next;
-            return head;
-        }
-        // cout<<count<<endl;
-        // if(n==1)
-        // while(cc!=(count-n)){
-        //     cc++;
-        //     // temp1=temp1->next;
-
-            
+        // ListNode* temp=head;
+        // ListNode* temp1=head;
+        // if(temp->next==NULL) return NULL;
+        // int count=1;
+        // while(temp->next->next!=NULL){
+        //     temp=temp->next;
+        //     count++;
+        //     // cout<<count<<endl;
         // }
-        // if(temp1->next==NULL){
+        // int del=count-n+1;
+        // int countt=1;
+        // if((count+1)==n){ 
+        //     head=head->next;
             
+        //     return head;}
+        // if(n==1){
+        //     temp->next=NULL;
+        //     return head;
         // }
-        // temp1->next=temp1-next->next;
-        // return head;
+        // else {
+        //     while(countt!=del){
+        //         temp1=temp1->next;
+        //          countt++;
+
+        //     }
+        //     temp1->next=temp1->next->next;
+        //     return head;
+        // }
+         ListNode* start = new ListNode();
+        start -> next = head;
+        ListNode* fast=start;
+        ListNode* slow= start;
+        for(int i=0;i<n;i++){
+            fast=fast->next;
+        }
+        // if(head==NULL && n==1){
+        //     return NULL;
+        // }
+        while(fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next;
+
+        }
+        slow->next=slow->next->next;
+        return start->next;
+       
+        
+        
 
     }
 
