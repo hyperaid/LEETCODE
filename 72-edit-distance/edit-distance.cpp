92e995b7490c7dc899dcee95866af072eb12bcc1
ABHINAV SHARMA <97845012+hyperaid@users.noreply.github.com>
2024-06-10 14:59:07 +0530

    Time: 47 ms (15.97%) | Memory: 82.3 MB (5.00%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -9,8 +9,8 @@ public:
         if(dp[n][m]!=-1) return dp[n][m];
          if(a[n-1]==b[m-1]) return dp[n][m]=solve(a,b,n-1,m-1,dp);
    
-        int insertion= solve(a,b,n,m-1,dp);
-        int delete1= solve(a,b,n-1,m,dp);
+        int insertion= solve(a,b,n-1,m,dp);
+        int delete1= solve(a,b,n,m-1,dp);
         int replace= solve(a,b,n-1,m-1,dp);    
         return dp[n][m]=1+min({insertion,delete1,replace});
         
