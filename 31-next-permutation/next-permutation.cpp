a7ad41749bf23e55cc9f1bc53fe01b1e755a0949
ABHINAV SHARMA <97845012+hyperaid@users.noreply.github.com>
2024-06-19 13:17:14 +0530

    Time: 4 ms (56.07%) | Memory: 14.5 MB (86.85%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int i=n-2;
+        int j=n-1;
+        if(n==1) return ;
+        while(i>=0&&j>0){
+            // if(i==0)
+            if(i==0){
+                if(nums[i]<nums[j] ){
+                    int k=nums.size();
+                    while(--k){
+                        if(nums[i]<nums[k]){
+                            break;
+                        }
+                    }
+                    swap(nums[i],nums[k]);
+                    reverse(nums.begin()+1,nums.end());
+                    return;
+                }
+
+            }
+            if(nums[i]<nums[j]){
+                int k=nums.size()-1;
+                while(k){
+                    if(nums[i]<nums[k]){
+                        break;
+                    }
+                    k--;
+                }
+                cout<<k<<endl;
+                swap(nums[i],nums[k]);
+                reverse(nums.begin()+i+1,nums.end());
+                return;
+            }
+            i--;
+            j--;
+        }
+        reverse(nums.begin(),nums.end());
+
+
+    }
+};
