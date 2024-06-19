28da21ff86234e6ef490c075e6b46e8855192878
ABHINAV SHARMA <97845012+hyperaid@users.noreply.github.com>
2024-06-19 13:17:33 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (54.27%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -7,20 +7,20 @@ public:
         if(n==1) return ;
         while(i>=0&&j>0){
             // if(i==0)
-            if(i==0){
-                if(nums[i]<nums[j] ){
-                    int k=nums.size();
-                    while(--k){
-                        if(nums[i]<nums[k]){
-                            break;
-                        }
-                    }
-                    swap(nums[i],nums[k]);
-                    reverse(nums.begin()+1,nums.end());
-                    return;
-                }
+            // if(i==0){
+            //     if(nums[i]<nums[j] ){
+            //         int k=nums.size();
+            //         while(--k){
+            //             if(nums[i]<nums[k]){
+            //                 break;
+            //             }
+            //         }
+            //         swap(nums[i],nums[k]);
+            //         reverse(nums.begin()+1,nums.end());
+            //         return;
+            //     }
 
-            }
+            // }
             if(nums[i]<nums[j]){
                 int k=nums.size()-1;
                 while(k){
