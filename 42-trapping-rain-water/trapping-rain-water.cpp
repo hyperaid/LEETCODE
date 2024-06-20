ca559ca02d7a4d3dc1a22514f9f55006910a1826
ABHINAV SHARMA <97845012+hyperaid@users.noreply.github.com>
2024-06-20 15:22:56 +0530

    Time: 31 ms (5.79%) | Memory: 23.3 MB (14.93%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    int trap(vector<int>& h) {
-        int n=h.size();
-        vector<int>v1(n);
-        vector<int>v2(n);
-        v1[0]=h[0];
-        v2[n-1]=h[n-1];
+    int trap(vector<int>& height) {
+        int n=height.size();
+        vector<int>prefix(n);
+        vector<int>suffix(n);
+        prefix[0]=height[0];
+        suffix[n-1]=height[n-1];
         for(int i=1;i<n;i++){
-            v1[i]=max(v1[i-1],h[i]);
+            prefix[i]=max(prefix[i-1],height[i]);
+
         }
         for(int i=n-2;i>=0;i--){
-            v2[i]=max(h[i],v2[i+1]);
+            suffix[i]=max(suffix[i+1],height[i]);
+            
         }
-        int count=0;
-
-        for(int i=0;i<h.size();i++){
-            count+=min(v1[i],v2[i])-h[i];
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int a=min(prefix[i],suffix[i]);
+            cout<<a<<"  "<<height[i];
+            if(height[i]<a){
+                ans+=(a-height[i]);
+            }
+            
         }
-        return count;
+        return ans;
     }
 };
