256baab408081c013958784e1104ad4b21dac902
ABHINAV SHARMA <97845012+hyperaid@users.noreply.github.com>
2024-06-21 14:26:05 +0530

    Time: 20 ms (37.84%) | Memory: 11.3 MB (49.66%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -7,26 +7,13 @@ public:
         int ans=0;
         int count=0;
         while(j<s.length() && i<s.length()){
-            // if(!(mpp.find(s[j])!=mpp.end()) && mpp[s[i]]==0){
-            //     while(mpp[s[j]]==0 && j<s.length()){
-            //         mpp[s[j]]++;
-            //         j++;
-            //     }
-            //     ans=max(ans,j-i+1);
-
-            // }
-            // else{
-            //     while(mpp[s[i]]>1 && i<s.length()){
-            //         mpp[s[i]]--;
-            //         i++;
-            //     }
-            // }
-
+            
             while(mpp[s[j]]==0 &&j<s.length()){
                 count++;
                 mpp[s[j]]++;
+                ans=max(ans,j-i+1);
+
                 j++;
-                ans=max(ans,j-i);
             }
             mpp[s[i]]--;
             i++;
