e5cc4aebd98b3f642920018a3026591e0f45ca50
ABHINAV SHARMA <97845012+hyperaid@users.noreply.github.com>
2024-07-11 12:57:34 +0530

    Time: 20 ms (72.87%) | Memory: 26.5 MB (64.44%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums2) {
-        stack<int>st;
-        int n=nums2.size();
-        // for()
-        vector<int>ans(n);
-       for(int i=2*nums2.size()-1;i>=0;i--){
-            while(!st.empty()&& st.top()<=nums2[i%n]) st.pop();
-
-            if(i<n){
-                if(!st.empty()) ans[i]=st.top();
-                else{
-                    ans[i]=-1;
-                }
+        stack<int> st;
+        vector<int> ans(nums2.size());
+        int n = nums2.size();
+        for (int i = 0; i < (2 * n) ; i++) {
+            while (!st.empty() && nums2[st.top()%n]<nums2[i % n]) {
+               ans[st.top()%n]=nums2[i%n];
+                st.pop();
             }
-            
-            st.push(nums2[i%n]);
+            // if (!st.empty()) {
+            //     nums2[i % n] = nums2[st.top()];
+            // }
+            st.push(i);
+        }
+        // cout<<st.size();
+        while (!st.empty()) {
+            if(st.top()<n){
+            ans[st.top()] = -1;
 
-
-       }
-       return ans;
+            }
+            st.pop();
+        }
+        return ans;
     }
 };
