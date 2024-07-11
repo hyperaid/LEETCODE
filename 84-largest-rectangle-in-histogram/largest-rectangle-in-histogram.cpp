0bc56160b52256bcae04d0740fbe22e6a435ae35
ABHINAV SHARMA <97845012+hyperaid@users.noreply.github.com>
2024-07-11 16:31:53 +0530

    Time: 136 ms (26.54%) | Memory: 88.1 MB (27.74%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,54 +1,50 @@
 class Solution {
 public:
-    void NSL(vector<int>&v1,stack<pair<int,int>>&s1,vector<int>& heights){
+    void nsr(vector<int>&v1,vector<int>& heights){
+        stack<int>st;
         for(int i=0;i<heights.size();i++){
-            if(s1.size()==0) v1.push_back(-1);
-            else if(s1.size()>0 && s1.top().first<heights[i]) v1.push_back(s1.top().second);
-            else if(s1.size()>0 && s1.top().first>=heights[i]){
-                while(s1.size()>0 && s1.top().first>=heights[i] ){
-                    s1.pop();
-                }
-                if(s1.size()==0) v1.push_back(-1);
-                else v1.push_back(s1.top().second);
-
+            while(!st.empty() && heights[st.top()]>heights[i]){
+                v1[st.top()]=i;
+                st.pop();
             }
-            s1.push({heights[i],i});
+            st.push(i);
+        
         }
+        while(!st.empty()){
+            v1[st.top()]=heights.size();
+            st.pop();
+        }
+
     }
-    void NSR(vector<int>&v2,stack<pair<int,int>>&s2,vector<int>& heights){
+    void nsl(vector<int>&v1,vector<int>& heights){
+        stack<int>st;
         for(int i=heights.size()-1;i>=0;i--){
-            if(s2.size()==0) v2.push_back(heights.size());
-            else if(s2.size()>0 && s2.top().first<heights[i]) v2.push_back(s2.top().second);
-            else {
-                while(s2.size()>0 && s2.top().first>=heights[i] ){
-                    s2.pop();
-                }
-                if(s2.size()==0) v2.push_back(heights.size());
-                else v2.push_back(s2.top().second);
-
+            while(!st.empty() && heights[st.top()]>heights[i]){
+                v1[st.top()]=i;
+                st.pop();
             }
-            s2.push({heights[i],i});
+            st.push(i);
+        
         }
-        reverse(v2.begin(), v2.end());
+        while(!st.empty()){
+            v1[st.top()]=-1;
+            st.pop();
+        }
 
     }
     int largestRectangleArea(vector<int>& heights) {
-        vector<int> v1;
-        vector<int>v2;
-        stack<pair<int,int>>s1;
-        stack<pair<int,int>>s2;
-        
-        NSL(v1,s1,heights);
-        NSR(v2,s2,heights);
-        vector<int>width(v1.size());
-        // width.push_back(-10);
-        for(int i=0;i<v1.size();i++){
-            cout<<v1[i]<<" "<<v2[i]<<endl;
-            width[i]=v2[i]-v1[i]-1;
-            width[i]=width[i]*heights[i];
-        }
-        return *max_element(width.begin(), width.end());
-        // return -1;
+        int n=heights.size();
+        vector<int>v1(n,-1);
+        vector<int>v2(n,-1);
+        int ans=INT_MIN;
+        nsr(v1,heights);
+        nsl(v2,heights);
+        for(int i=0;i<n;i++){
+            // if(v1[i]==-1 && v)
+            int w=v1[i]-v2[i]-1;
 
+            ans=max(ans,w*heights[i]);
+        }
+        return ans;
     }
 };
