bee96932d8b39db26728bd2e994ec14503c78522
ABHINAV SHARMA <97845012+hyperaid@users.noreply.github.com>
2024-08-13 12:52:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (8.15%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-     
-   
-        int n= nums.size();
-        vector<int> sum(n);
-        if(n==1) return nums[0];
-        if(n==2) return max(nums[0],nums[1]);
-        sum[0]=nums[0];
-        sum[1]=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            sum[i]=max(sum[i-1],sum[i-2]+nums[i]);
+    int solve(int i,vector<int>&nums,vector<int>&dp){
+        if(i==0){
+            return nums[0]; 
         }
-        return sum[n-1];
+        if(i==1) return max(nums[1],nums[0]);
+        if(dp[i]!=-1) return dp[i];
+        int take=INT_MIN;
+        if(i-2>=0){
+            take=nums[i]+solve(i-2,nums,dp);
+        }
+        int n_take=solve(i-1,nums,dp);
+        return dp[i]=max(take,n_take);
     }
-    
+    int rob(vector<int>& nums) {
+        int n=nums.size()-1;
+        // if((n+1)==2) return max(nums[0],nums[1])
+        vector<int>dp(n+1,-1);
+        return solve(n,nums,dp);
+    }
 };
